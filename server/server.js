0c0fabda17293bfd5048e84fbd7d846f73fccde0
Akash Kumar <88208432+akash2299kumar@users.noreply.github.com>
2024-10-28 23:22:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,13 +11,13 @@ const boardRoutes = require("./routes/boardRoutes");
 const cors = require("cors");
 
 
-app.use(
-  cors({
-    origin: ["https://akash22992000-gmail-com-cuvette-final-evaluation-feb-client.vercel.app"], 
-    methods:  ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://akash22992000-gmail-com-cuvette-final-evaluation-feb-client.vercel.app"], 
+//     methods:  ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//   })
+// );
 
 // const frontendURL = 'https://akash22992000-gmail-com-cuvette-final-evaluation-feb-client.vercel.app';
 
@@ -25,10 +25,16 @@ app.use(
 //   origin: frontendURL,
 //   credentials: true, // Enable this if you are sending cookies or authorization headers
 // }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://akash22992000-gmail-com-cuvette-final-evaluation-feb-client.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 
-
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json());
 app.use("/api/users", usersRoute);
